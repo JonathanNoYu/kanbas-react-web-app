7fa50cbbdccd49ebfafe5beab41f395d46c268b7
Jonathan Yu <yu.jona@northeastern.edu>
2023-12-04 12:55:12 -0500

    Updated axios to request in users client.js

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,8 +1,4 @@
 import axios from "axios";
-// const COURSES_URL = "https://kanbas-node-server-app-xrhy.onrender.com/api/courses";
-// const MODULES_URL = "https://kanbas-node-server-app-xrhy.onrender.com/api/modules";
-// const COURSES_URL = "http://localhost:4000/api/courses";
-// const MODULES_URL = "http://localhost:4000/api/modules";
 const API_BASE = process.env.REACT_APP_BASE_API_URL;
 const MODULES_URL = `${API_BASE}/modules`;
 const COURSES_URL = `${API_BASE}/courses`;
